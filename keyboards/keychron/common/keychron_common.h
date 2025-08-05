86a2def4b02b2ed07a2413baf21be17581bf4862
vx09 <ceo@webnyo.com>
2025-08-05 05:04:21 +0200

    perf: created two custom keycodes
    instead of passing normal keycodes through a loop to check their
    position, you just created two new keycodes and placed them there.
    
    That saves an if statement.

--- keyboards/keychron/common/keychron_common.h
+++ keyboards/keychron/common/keychron_common.h
@@ -35,7 +35,9 @@ enum custom_keycodes {
     KC_TASK_VIEW,
     KC_FILE_EXPLORER,
     KC_SCREEN_SHOT,
-    KC_CORTANA
+    KC_CORTANA,
+    CTL_QUOT,
+    CTL_ESC = SAFE_RANGE
 };
 
 #define KC_TASK KC_TASK_VIEW
--- keyboards/keychron/q8/ansi_encoder/keymaps/vial/keymap.c
+++ keyboards/keychron/q8/ansi_encoder/keymaps/vial/keymap.c
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [WIN_BASE] = LAYOUT_ansi_69(
         KC_ESC,  KC_1,     KC_2,     KC_3,    KC_4,    KC_5,    KC_6,     KC_7,     KC_8,    KC_9,    KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,             KC_MUTE,
         KC_TAB,  KC_Q,     KC_W,     KC_E,    KC_R,    KC_T,    KC_Y,     KC_U,     KC_I,    KC_O,    KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,             KC_DEL,
-        KC_CAPS, KC_A,     KC_S,     KC_D,    KC_F,    KC_G,              KC_H,     KC_J,    KC_K,    KC_L,     KC_SCLN,  KC_QUOT,  KC_ENT,              KC_HOME,
+        CTL_ESC, KC_A,     KC_S,     KC_D,    KC_F,    KC_G,              KC_H,     KC_J,    KC_K,    KC_L,     KC_SCLN,  CTL_QUOT,  KC_ENT,              KC_HOME,
         OSM(MOD_LSFT),     KC_Z,     KC_X,    KC_C,    KC_V,    KC_B,     KC_B,     KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  OSM(MOD_RSFT), KC_UP,
         KC_LCTL, KC_LWIN,  KC_LALT,           KC_SPC,           MO(_FN2), MO(_FN3),          KC_SPC,            KC_RALT,            KC_LEFT, KC_DOWN,    KC_RGHT),
 
@@ -81,18 +81,16 @@ void housekeeping_task_user(void) {
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    // handle Keychron processing (original code)
     if (!process_record_keychron(keycode, record)) {
         return false;
     }
 
-    // Handle our custom mod-tap keys
     static bool ctl_esc_active = false;
     static bool ctl_quote_active = false;
     static bool other_pressed = false;
 
     switch (keycode) {
-        case KC_CAPS:  // Ctrl/Esc on Caps Lock
+        case CTL_ESC:
             if (record->event.pressed) {
                 ctl_esc_active = true;
                 register_code(KC_LCTL);
@@ -104,7 +102,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             }
             return false;
 
-        case KC_QUOT:  // Ctrl/' on Quote key
+        case CTL_QUOT:
             if (record->event.pressed) {
                 ctl_quote_active = true;
                 register_code(KC_LCTL);
@@ -117,7 +115,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             return false;
 
         default:
-            // Track if any other key was pressed while our mods were active
             if (record->event.pressed && (ctl_esc_active || ctl_quote_active)) {
                 other_pressed = true;
             }
