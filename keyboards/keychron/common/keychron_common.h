1f1b67db9a6afa9f2a3978ebc43784f352524749
vx09 <ceo@webnyo.com>
2025-09-03 08:41:13 +0200

    Revert "perf: created two custom keycodes"
    This reverts commit 86a2def4b02b2ed07a2413baf21be17581bf4862.

--- keyboards/keychron/common/keychron_common.h
+++ keyboards/keychron/common/keychron_common.h
@@ -35,9 +35,7 @@ enum custom_keycodes {
     KC_TASK_VIEW,
     KC_FILE_EXPLORER,
     KC_SCREEN_SHOT,
-    KC_CORTANA,
-    CTL_QUOT,
-    CTL_ESC = SAFE_RANGE
+    KC_CORTANA
 };
 
 #define KC_TASK KC_TASK_VIEW
