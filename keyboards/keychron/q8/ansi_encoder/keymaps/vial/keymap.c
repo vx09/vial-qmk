88ba2f450099ce0885e85f64b677866e51aa1cfc
vx09 <ceo@webnyo.com>
2025-09-03 07:34:44 +0200

    feat: OSM, LT, and tap-hold.
    most importantly, tap hold replaces the custom keys you wrote since now
    it is possible to use the QMK equivalent retro_tapping and even better version, permissive_hold.

--- keyboards/keychron/q8/ansi_encoder/keymaps/vial/keymap.c
+++ keyboards/keychron/q8/ansi_encoder/keymaps/vial/keymap.c
@@ -27,6 +27,12 @@ enum layers{
     _FN3
 };
 
+// IMPORTANT!: THIS FIRMWARE IS HALF BAKED (combos and settings like permissive_hold have to be configured in vial interface for example).
+// Vial is designed so you don't need to reflash the firmware unless something goes very wrong.
+// Your vial config has everything, and it is backed up to a layout.vil file in your ./Sync/keyboards/keychron-q8 folder.
+// In case your vial config failed and need to reflash, you still did half of the job here in this file.
+// After reflashing you would need to add the nav layer and the combos.
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [MAC_BASE] = LAYOUT_ansi_69(
         KC_ESC,  KC_1,     KC_2,     KC_3,    KC_4,    KC_5,    KC_6,     KC_7,     KC_8,    KC_9,    KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,             KC_MUTE,
@@ -38,9 +44,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [WIN_BASE] = LAYOUT_ansi_69(
         KC_ESC,  KC_1,     KC_2,     KC_3,    KC_4,    KC_5,    KC_6,     KC_7,     KC_8,    KC_9,    KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,             KC_MUTE,
         KC_TAB,  KC_Q,     KC_W,     KC_E,    KC_R,    KC_T,    KC_Y,     KC_U,     KC_I,    KC_O,    KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,             KC_DEL,
-        CTL_ESC, KC_A,     KC_S,     KC_D,    KC_F,    KC_G,              KC_H,     KC_J,    KC_K,    KC_L,     KC_SCLN,  CTL_QUOT,  KC_ENT,              KC_HOME,
+        LCTL_T(KC_ESC), KC_A,     KC_S,     KC_D,    KC_F,    KC_G,              KC_H,     KC_J,    KC_K,    KC_L,     KC_SCLN,  RCTL_T(KC_QUOT),  KC_ENT,              KC_HOME,
         OSM(MOD_LSFT),     KC_Z,     KC_X,    KC_C,    KC_V,    KC_B,     KC_B,     KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  OSM(MOD_RSFT), KC_UP,
-        KC_LCTL, KC_LWIN,  KC_LALT,           KC_SPC,           MO(_FN2), MO(_FN3),          KC_SPC,            KC_RALT,            KC_LEFT, KC_DOWN,    KC_RGHT),
+        KC_LCTL, KC_LWIN,  OSM(MOD_LALT),           KC_SPC,           LT3(KC_CAPS), LT4(KC_APPLICATION),          KC_SPC,         OSM(MOD_RALT),            KC_LEFT, KC_DOWN,    KC_RGHT),
 
     [_FN1] = LAYOUT_ansi_69(
         KC_GRV,  KC_BRID,  KC_BRIU,  KC_MCTL, KC_LPAD, RGB_VAD, RGB_VAI,  KC_MPRV,  KC_MPLY, KC_MNXT, KC_MUTE,  KC_VOLD,  KC_VOLU,  _______,             RGB_TOG,
